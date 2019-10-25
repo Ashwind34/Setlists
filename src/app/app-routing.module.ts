d6721594bdaab04754e5c6ac535e5fed16cfaad0
Ashwind34 <ahester34@gmail.com>
2019-10-24 22:27:05 -0700

    initial setup. created setlist component.  added angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SetlistComponent } from './setlist/setlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'setlist', pathMatch: 'full'},
+  {path: 'setlist', component: SetlistComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
